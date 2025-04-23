6cfba01b5ab4e1394fcc2f1684047b67118fce2f
Manh Blue <manhducnb204@gmail.com>
2025-04-23 23:48:46 +0700

    lan2

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -2,7 +2,6 @@ const Task = require("../models/task.model");
 
 const paginationHelper = require("../../../helpers/pagination");
 const searchHelper = require("../../../helpers/search");
-const { search } = require("../routes/task.route");
 
 //[GET] api/v1/tasks/
 module.exports.index = async (req, res) => {
@@ -70,27 +69,138 @@ module.exports.detail = async (req, res) => {
 
 //[PATCH] api/v1/tasks/change-status/:id
 module.exports.changeStatus = async (req, res) => {
-    try{
-      const id = req.params.id;
-      const status = req.body.status;
-  
-      await Task.updateOne({
-        _id: id
-      },{
-        status: status
-      });
-  
-      res.json({
-        code: 200,
-        message: "Cập nhật trạng thái thành công!"
-      });
-    }
-    catch(error){
-      res.json({
-        code: 400,
-        message: "khong ton tai!"
-      });
-    }
+  try {
+    const id = req.params.id;
+    const status = req.body.status;
+
+    await Task.updateOne({
+      _id: id
+    }, {
+      status: status
+    });
+
+    res.json({
+      code: 200,
+      message: "Cập nhật trạng thái thành công!"
+    });
+  }
+  catch (error) {
+    res.json({
+      code: 400,
+      message: "khong ton tai!"
+    });
+  }
 };
+
+//[PATCH] api/v1/tasks/change-multi
+module.exports.changeMulti = async (req, res) => {
+  try {
+    const { ids, key, value } = req.body;
+
+    switch (key) {
+      case "status":
+        await Task.updateMany({
+          _id: { $in: ids }
+        }, {
+          status: value
+        });
+        res.json({
+          code: 200,
+          message: "Cập nhật trạng thái thành công!"
+        });
+        break;
+
+
+      case "delete":
+        await Task.updateMany({
+          _id: { $in: ids }
+        }, {
+          deleted: true,
+          deleteAt: new Date()
+        });
+        res.json({
+          code: 200,
+          message: "Xoa thành công!"
+        });
+        break;
+        
+
+      default:
+        res.json({
+          code: 400,
+          message: "khong ton tai!"
+        });
+        break;
+    }
+  }
+  catch (error) {
+    res.json({
+      code: 400,
+      message: "Khong ton tai!"
+    });
+  }
+};
+
+//[POST] api/v1/tasks/create
+module.exports.create = async (req, res) => {
+  try {
+    const task = new Task(req.body);
+    const data = await task.save();
+
+    res.json({
+      code: 200,
+      message: "Tao thanh cong!",
+      data: data
+    });
+  }
+  catch (error) {
+    res.json({
+      code: 400,
+      message: "Loi"
+    });
+  }
+};
+
+//[PATCH] api/v1/tasks/edit/:id
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Task.updateOne({ _id: id }, req.body);
+
+    res.json({
+      code: 200,
+      message: "Cap nhat thanh cong!"
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Loi!"
+    });
+  }
+};
+
+//[DELETE] api/v1/tasks/delete/:id
+module.exports.delete = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await Task.updateOne({ _id: id }, {
+      deleted: true,
+      deleteAt: new Date()
+    });
+
+    res.json({
+      code: 200,
+      message: "Xoa thanh cong!"
+    });
+  } catch (error) {
+    res.json({
+      code: 400,
+      message: "Loi!"
+    });
+  }
+};
+
 
 
